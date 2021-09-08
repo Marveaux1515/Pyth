e1355b8007ad9dea55347ff36efc427d1719fb48
Marveaux1515 <65611082+Marveaux1515@users.noreply.github.com>
2021-09-08 03:04:05 +0100

    Delete config.py

--- Sidegig/config.py
+++
@@ -1,4 +0,0 @@
-SIDEGIG_DETAILS = {"user_name":["Marveaux","Mvx","Marv07","Marx07"],
-"password":["Dunce1515","De1515","Asanwa0723","De1515"]}
-INSTAGRAM_DETAILS = {"user_name":[["marveaux1515","mvx15150"],["nivram15150","mjasa1507"],["mavx2307","mjax07"],
-"marx0723"], "password":[[]]}
