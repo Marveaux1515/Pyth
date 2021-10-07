e7799e3b49a6ae9f1d78f3f561dabc1b1d72d285
Marveaux1515 <marvenwes@gmail.com>
2021-10-07 03:45:30 +0100

    modified error catching in the Locator.locate()

--- Sidegig/Utils.py
+++ Sidegig/Utils.py
@@ -16,36 +16,44 @@ class Locator():
         try:
             WebDriverWait(self.driver, self.wait_time).until(
                     EC.presence_of_element_located((style_dict[style_type][0],style_dict[style_type][2])))
-            web_element=style_dict[style_type][1]
+            web_element=eval(style_dict[style_type][1])
             return web_element
-        except:
+        except Exception as e:
+            print(f"refresh exception as {e}")
             return False
-    def locate(self,style_type,styling,base=False):
-        style_dict={'Id':[By.ID,self.driver.find_element_by_id(styling),styling],
-                    'Name':[By.NAME,self.driver.find_element_by_name(styling),styling],
-                    'Css':[By.CSS_SELECTOR,self.driver.find_elements_by_css_selector(styling),styling],
-                    'Tag_name':[By.TAG_NAME,self.driver.find_elements_by_tag_name(styling),styling]}
+    def locate(self,style_type,styling,base=False,refresh=True):
+        style_dict={'Id':[By.ID,"self.driver.find_element_by_id(styling)",styling],
+                    'Name':[By.NAME,"self.driver.find_element_by_name(styling)",styling],
+                    'Css':[By.CSS_SELECTOR,"self.driver.find_elements_by_css_selector(styling)",styling],
+                    
+                    'Tag_name':[By.TAG_NAME,"self.driver.find_elements_by_tag_name(styling)",styling]}
         if base:
             limit=np.inf()
         else:
-            limit=5
+            limit=2
         count=0
         try:
             WebDriverWait(self.driver, self.wait_time).until(
                     EC.presence_of_element_located((style_dict[style_type][0],style_dict[style_type][2])))
-            web_element=style_dict[style_type][1]
+            web_element=eval(style_dict[style_type][1])
             return web_element
-        except:
-            islocated=self.refresh(style_dict,style_type)
-            while not islocated or count <=limit:
+        except Exception as e:
+            print(f"location exception as {e}")
+            if refresh:
                 islocated=self.refresh(style_dict,style_type)
-                count+=1
-                time.sleep(5)
+                while  not islocated and count <=limit:
+                    islocated=self.refresh(style_dict,style_type)
+                    count+=1
+                    time.sleep(5)
+                else:
+                    return islocated
             else:
-                return islocated
+                return False
     def input_(self,input_details,style_type,styling):
         for i in range(len(input_details)):
+            print(style_type[i])
             elem=self.locate(style_type[i],styling[i])
+            
             if elem:
                 elem.send_keys(input_details[i])
             else:
@@ -53,15 +61,33 @@ class Locator():
         return True
     def click(self,style_type,styling):
         elem=self.locate(style_type,styling)
+        style_dict={'Id':[By.ID,"self.driver.find_element_by_id(styling)",styling],
+                    'Name':[By.NAME,"self.driver.find_element_by_name(styling)",styling],
+                    'Css':[By.CSS_SELECTOR,"self.driver.find_elements_by_css_selector(styling)",styling],
+                    'Tag_name':[By.TAG_NAME,"self.driver.find_elements_by_tag_name(styling)",styling]}
         if elem:
-            ActionChains(self.driver).move_to_element(elem).click().perform()
+            actions=ActionChains(self.driver)
+            if isinstance(elem,list):
+                try:
+                    elem[0].click()
+                except:
+                    actions.move_to_element(elem[0]).click().perform()
+            else:
+                try:
+                    elem.click()
+                except:
+                     actions.move_to_element(elem).click().perform()
         else:
             return False
         return True
     def get_attribute_(self,attrib,style_type,styling):
         elem=self.locate(style_type,styling)
         if elem:
-            attr=elem.get_attribute(attrib)
+            if isinstance(elem,list):
+                attr=elem[0].get_attribute(attrib)
+                
+            else:
+                attr=elem.get_attribute(attrib)
         else:
             return False
         return attr
